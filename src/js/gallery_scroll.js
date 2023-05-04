70b7dd9161868510b5834595b5bef3f2d8a057d6
WOWmasterJulia <topazzz@ukr.net>
2023-05-04 14:27:01 +0300

    Changes

--- src/js/gallery_scroll.js
+++ src/js/gallery_scroll.js
@@ -12,7 +12,7 @@ const pixabayAPI = new PixabayAPI();
 const searchFormEl = document.querySelector('.search-form');
 // const btnEl = document.querySelector('.load-more');
 searchFormEl.addEventListener('submit', onSearchForm);
-
+let ttlPic = 0;
 const perPage = 40;
 let totalPage = 0;
 let page = 1;
@@ -20,34 +20,38 @@ let gallery = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,  
 });
+
 const optionsScroll = {
     root: null,
     rootMargin: '300px',
     threshold: 1.0,
 }
-const callback = async function (entries, observer) {
+const callback = async function (entries, obs) {
     console.log(entries[0].isIntersecting);
     try {
         if (entries[0].isIntersecting) {
                 page += 1;
-                observer.disconnect();
-                const respons = await pixabayAPI.getPhotoByQuery(page);
+            obs.disconnect();
+            if (page <= totalPage) {
+                const respons = await pixabayAPI.getPhotoByQuery(page, perPage);
                 galleryEl.insertAdjacentHTML('beforeend', createGalleryCards(respons.data.hits));
-                observer.observe(galleryEl.lastElementChild);
-                console.log(page, totalPage);
-                if (page === totalPage) {
-                    Report.failure("We're sorry, but you've reached the end of search results.");
-                    // btnEl.classList.add('is-hidden');
-                    observer.disconnect();
-                    return;
-                }
+                ttlPic = ttlPic + respons.data.hits.length;
+                obs.observe(galleryEl.lastElementChild);
+                console.log(page, totalPage, ttlPic);
+            }   else {
+            // if (page === totalPage) {
+                Report.failure("We're sorry, but you've reached the end of search results.");
+                // btnEl.classList.add('is-hidden');
+                obs.disconnect();
+                return;
+            }
         }
       } catch (error) {
     console.log(error.message);
   }      
 };
-
 const observer = new IntersectionObserver(callback, optionsScroll);
+
 /* Пошук по запиту */
 // console.log(observer);
 async function onSearchForm(event) {
@@ -62,10 +66,12 @@ async function onSearchForm(event) {
         return Report.warning('Empty request!!!', 'fill in the request field!')
     }
     galleryEl.innerHTML = '';
-    page = 1;
+    page = 1; // сброс счетчика страниц
+    ttlPic = 0; //сброс счетчика карточек 
     try {   
         const respons = await pixabayAPI.getPhotoByQuery(page,perPage);
-        // console.log(respons);
+        ttlPic = respons.data.hits.length + ttlPic;
+        console.log(ttlPic);
         if (respons.data.hits.length === 0) {
             Report.info("Sorry, there are no images matching your search query. Please try again.");
         } else {
